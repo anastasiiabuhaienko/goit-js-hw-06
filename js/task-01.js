f0ef58f477545bff0d639f5f6238783d9e02db63
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-02-12 20:17:27 +0200

    find categories

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const listWithID = document.querySelector('#categories');
+// console.log(listWithID);
+
+
+
+const listCategories = document.querySelectorAll('h2');
+const countCategories = listCategories.length;
+
+console.log('Number of categories: ', countCategories);
