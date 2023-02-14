687434963144a7dab513ef22e5db329bf19cb525
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-02-14 21:27:12 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,18 @@
-const listWithID = document.querySelector('#categories');
-// console.log(listWithID);
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка
+// элемента(тега < h2 >) и количество элементов в категории(всех вложенных в него < li >).
 
 
+const listCategoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listCategoriesEl.length}`);
 
-const listCategories = document.querySelectorAll('h2');
-const countCategories = listCategories.length;
 
-console.log('Number of categories: ', countCategories);
+listCategoriesEl.forEach(function (category, index) {
+    console.log('\n');
+    console.log(`Category: ${listCategoriesEl[index].firstElementChild.textContent}`);
+    console.log(`Elements: ${listCategoriesEl[index].lastElementChild.childElementCount}`);
+
+
+});
+
