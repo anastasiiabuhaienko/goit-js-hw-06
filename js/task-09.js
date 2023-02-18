025b4d39618db2c2a47a087a4bf9e3b61d3692d8
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-02-18 19:23:03 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const btnColor = document.querySelector('button');
+const spanEl = document.querySelector('span');
+
+
+btnColor.addEventListener('click', (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  const color = body.style.backgroundColor;
+  spanEl.innerHTML = `- ${color}`;
+});
+
