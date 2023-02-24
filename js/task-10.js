e000a7fcd80318a31bd953956fa5a5690c8f2b98
Anastasiia_Buhaienko <anastasiia.buhaienko@gmail.com>
2023-02-24 13:43:23 +0200

    fix task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,31 +2,23 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const inpArea = document.querySelector('input');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
-
-//добавляем слушателя
 inpArea.addEventListener('input', getBoxAmount);
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destroyBoxes);
-
-
 let boxWidth = 20;
 let boxHeight = 20;
-
-//Узнаем переданое число
 
 function getBoxAmount(event) {
 inpArea.setAttribute('amount', event.currentTarget.value); 
 };
 
-
 function createBoxes(amount) {
- let amountBoxes = inpArea.getAttribute('amount');
-  
+  let amountBoxes = inpArea.getAttribute('amount');
+  const arr = [];
   for (let i = 0; i < amountBoxes; i += 1) {
     boxWidth += 10;
     boxHeight += 10;   
@@ -36,10 +28,13 @@ function createBoxes(amount) {
     box.style.height = `${boxHeight}px`;    
     box.classList.add('box');
     box.style.background = getRandomHexColor();    
-    boxes.append(box);
-};
 
-    inpArea.value = '';
+    arr.push(box);
+  };
+  
+  inpArea.value = '';
+  boxes.append(...arr);
+
 };
 
 function destroyBoxes(event) {  
